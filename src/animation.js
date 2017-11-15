1ab4e9a395896ee0ed2cad18bc89f4912c35d449
lijinrong <2561071197@qq.com>
2017-11-15 23:54:09 +0800

    yunche

--- src/animation.js
+++ src/animation.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var loadImage = require('./imageLoader');
+var Timeline = require('./Timeline');
 
 //初始化状态
 var STATE_INITIAL = 0;
@@ -17,6 +18,11 @@ var TASK_SYNC= 0;
 // 异步任务
 var TASK_ASYC= 1;
 
+//执行函数callback
+function next(callback){
+	callback && callback();
+}
+
 /**
 动画库类
 *@constructor
@@ -24,6 +30,7 @@ var TASK_ASYC= 1;
 function Animation(){
 	this.taskQueue = [];// 任务链
 	this.index = 0;// 任务索引
+	this.timeline = new Timeline();
 	this.state = STATE_INITIAL;
 }
 
@@ -42,13 +49,41 @@ Animation.prototype.loadImage = function(imglist){
 };
 
 /**
-*改变图片背景位置
+*添加一个异步定时任务，改变图片背景位置
 *@el image标签
 *@positions 背景位置数组
 *@imgUrl 图片地址
 */
 Animation.prototype.changePosition = function(el, positions, imgUrl){
+	var len = positions.length;
+	var taskFn;
+	var type;
+	if(len){
+		var me = this;
+		taskFn = function(next, time){
+			if(imgUrl){
+				ele.style.backgroundImage = 'url(' + imgUrl + ')';
+			}
 
+			// 获得当前背景图片位置索引
+			var index = Math.min(time/me.interval|0,len - 1); //time/me.interval看是第几次，|0是取整
+			var position = positions[index].spit(' ');
+
+			//改变dom对象的背景图片位置
+			ele.style.backgroundPosition = position[0] + 'px ' + position[1] + 'px';
+
+			// 图片到最后一帧时，执行下一个任务
+			if(index === len - 1){
+				next();
+			}
+		};
+		type = TASK_ASYC;
+	}else{
+		taskFn = next;
+		type = TASK_SYNC;
+	}
+
+	return this._add(taskFn,type);
 };
 
 /**
@@ -57,38 +92,85 @@ Animation.prototype.changePosition = function(el, positions, imgUrl){
 *@imglist 图片数组
 */
 Animation.prototype.changeSrc = function(el, imglist){
+	var len = imglist.length;
+	var taskFn;
+	var type;
 
+	if(len){
+		var me = this;
+		taskFn = function(next,time){
+			// 获得当前图片索引
+			var index = Math.min(time/me.interval | 0, len-1);
+
+			//改变image对象的图片地址
+			ele.src = imglist[index];
+			if(index === len -1){
+				next();
+			}			
+		}
+		type = TASK_ASYC;
+	}else{
+		taskFn = next;
+		type = TASK_SYNC;
+	}
+
+	return this._add(taskFn, type);
 };
 
 /**
 *每帧动画执行的函数
 *@callback
 */
-Animation.prototype.enterFrame = function(callback){
-
+Animation.prototype.enterFrame = function(taskFn){
+	return this._add(taskFn, TASK_ASYC);
 };
 
 /**
-*动画循环的次数
+*动画循环的次数 (将索引值回到上一个)
 *@times 循环次数
 */
 Animation.prototype.repeat = function(times){
+	var me = this;
+	var taskFn = function(){
+		if(typeof times === 'undefined'){
+			// 无线回退到上一个任务
+			me.index--;
+			me._runTask();
+			return;
+		}
 
+		if(times){
+			times--;
+			// 回退
+			me.index--;
+			me._runTask();
+		}else{
+			//达到了重复次数，跳转到下一个任务
+			var task = me.taskQueue[me.index];
+			me._next(task);
+		}
+	}
+
+	var type = TASK_SYNC;
+	return this._add(taskFn, type);
 };
 
 /**
-*无限重复上一次动画
+*无限重复上一次动画 
 */
 Animation.prototype.repeatForever = function(){
-
+	return this.repeat();
 };
 
 /**
-*每个动画执行完后等待的时间
+*每个动画执行完后等待的时间 给一个任务绑定一个等待时长
 @time 等待的时间
 */
 Animation.prototype.wait = function(time){
-
+	if(this.taskQueue && this.taskQueue.length > 0){
+		this.taskQueue[this.taskQueue.length-1].wait = time;
+	}
+	return this;
 };
 
 /**
@@ -96,7 +178,14 @@ Animation.prototype.wait = function(time){
 *@callback 回调函数
 */
 Animation.prototype.then = function(callback){
+	var taskFn = function(next){
+		callback();
+		next();
+	};
 
+	var type = TASK_SYNC;
+
+	return this._add(taskFn,type);
 };
 
 /**
@@ -123,21 +212,38 @@ Animation.prototype.start = function(interval){
 *动画暂停
 */
 Animation.prototype.pause = function(){
-
+	if(this.state === STATE_START){
+		this.state = STATE_STOP;
+		this.timeline.stop();
+		return this;
+	}
+	return this;
 };
 
 /**
 *动画从上一次暂停处重新执行
 */
 Animation.prototype.restart = function(){
-
+	if(this.state === STATE_STOP){
+		this.state = STATE_START;
+		this.timeline.restart();
+		return this;
+	}
+	return this;
 };
 
 /**
 *释放资源
 */
 Animation.prototype.dispose = function(){
-
+	if(this.state !== STATE_INITIAL){
+		this.state = STATE_INITIAL;
+		this.taskQueue = null;
+		this.time.stop();
+		this.timeline = null;
+		return this;
+	}
+	return this;
 };
 
 /**
@@ -190,7 +296,7 @@ Animation.prototype._syncTask = function (task) {
 	var me = this;
 	var next = function () {
 		//切换到下一个任务
-		me._next();
+		me._next(task);
     };
 	var taskFn = task.taskFn;
 	taskFn(next);//taskFn执行完后会执行next方法，切到下一个任务
@@ -202,14 +308,38 @@ Animation.prototype._syncTask = function (task) {
  * @private
  */
 Animation.prototype._asynTask = function (task) {
-	
+	var me = this;
+	//定义每一帧执行的回调函数
+	var enterFrame = function(time){
+		var taskFn = task.taskFn;
+		var next = function(){
+			// 停止当前任务
+			me.timeline.stop();
+
+			//执行下一个任务
+			me._next(task);
+		};
+		taskFn(next,time);
+	};
+	this.timeline.onenterframe = enterFrame;
+	this.timeline.start(this.interval);
 };
 
 /**
- * 切换到下一个任务
+ * 切换到下一个任务,支持如果当前任务需要等待，则延时执行
+ * task 当前任务
  * @private
  */
-Animation.prototype._next = function () {
+Animation.prototype._next = function (task) {
 	this.index++;
-	this._runTask();
+
+	var me = this;
+
+	task.wait ? setTimeout(function(){
+		me._runTask();
+	},task.wait) : this._runTask();
 };
+
+module.exports = function(){
+	return new Animation();
+}
